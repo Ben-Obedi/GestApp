610cdc0372ec149fa6c984f79e62c0f45a872e82
Ben-Obedi <benobedi61@gmail.com>
2025-05-14 00:11:31 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,30 +1,17 @@
-
-import 'package:flutter/material.dart';
-
 void main() {
   runApp(MyApp());
 }
+
+import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gid App',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Colors.blue[900],
-        scaffoldBackgroundColor: Colors.blueGrey[900],
+      home: Scaffold(
+        appBar: AppBar(title: Text('Gid App')),
+        body: Center(child: Text('Bienvenue sur Gid !')),
       ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Bienvenue sur Gid')),
-      body: Center(child: Text('Application en cours de développement...')),
     );
   }
 }
